64feb087b153e8062b4fda3424f9641392c5c386
tony2tones <anthony@globalkinetic.com>
2017-10-09 09:53:51 +0200

    sorting out Routing for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,39 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroesService }  from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    DashboardComponent.forRoot([
+      {
+        path:'',
+        redirectTo:'/dashboard',
+        pathMatch: 'full'
+      },
+    ])
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
