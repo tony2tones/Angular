fa384c7d94c016abf354c97d818f2bba06ee3247
tony2tones <anthony.marque@gmail.com>
2017-12-04 15:54:14 +0200

    managed to capture weather via geolocation now extracting weather details.

--- gkWeather/js/app.js
+++ gkWeather/js/app.js
@@ -4,17 +4,20 @@ $(document).ready(function () {
     if (navigator.geolocation) {
         var gl = navigator.geolocation;
         gl.getCurrentPosition(function (position) {
-            $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
             lat = position.coords.latitude;
             lon = position.coords.longitude;
+            
+
+            var key = '53f9d8e4213222cf517d86dc406d67fc';
+            var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key;
+
+            $.getJSON(api, function (data) {
+                var weatherType = data.weather[0].description;
+                console.log(api);
+                console.log(weatherType);
+                $("#data").html("weather type: " + weatherType);
+            });
         });
     }
-    //http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&
-    var key = '53f9d8e4213222cf517d86dc406d67fc';
-    var api = 'http://api.openweathermap.org/data/2.5/weather?' + 'lat=&' + lat + 'lon=&' + lon + '&appid=' + key;
-
-    $.getJSON(api, function (data) {
-        alert(data.coord.lon);
-    });
-
 });
+
