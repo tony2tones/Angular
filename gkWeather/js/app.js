1f0825340338ac49bea2f1931e7f7031fa53c65b
tony2tones <anthony.marque@gmail.com>
2017-12-04 16:02:08 +0200

    added Celcuis request to the API

--- gkWeather/js/app.js
+++ gkWeather/js/app.js
@@ -6,16 +6,16 @@ $(document).ready(function () {
         gl.getCurrentPosition(function (position) {
             lat = position.coords.latitude;
             lon = position.coords.longitude;
-            
-
             var key = '53f9d8e4213222cf517d86dc406d67fc';
-            var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key;
+            var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key + '&mode=json&units=metric';
 
             $.getJSON(api, function (data) {
                 var weatherType = data.weather[0].description;
-                console.log(api);
+                var temp = data.main.temp;
+                console.log(temp);
                 console.log(weatherType);
-                $("#data").html("weather type: " + weatherType);
+
+                $("#data").html("weather type: " + weatherType + ' ' + temp);
             });
         });
     }
