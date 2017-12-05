c11701788d389ece2ea30c475ff89ba94ca2e5b1
tony2tones <anthony.marque@gmail.com>
2017-12-05 11:47:44 +0200

    implemented Vue.js

--- gkWeather/js/app.js
+++ gkWeather/js/app.js
@@ -1,23 +1,69 @@
-$(document).ready(function () {
-    var lon;
-    var lat;
-    if (navigator.geolocation) {
-        var gl = navigator.geolocation;
-        gl.getCurrentPosition(function (position) {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            var key = '53f9d8e4213222cf517d86dc406d67fc';
-            var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key + '&mode=json&units=metric';
+new Vue({
+    el: '#data',
+    data: {
+        locationDetails:[
+            {
+                message: 'Hello guuys',
+                weatherType: 'tooa',
+                temp: ''
+            }
+        ]
+    },
 
-            $.getJSON(api, function (data) {
-                var weatherType = data.weather[0].description;
-                var temp = data.main.temp;
-                console.log(temp);
-                console.log(weatherType);
+    mounted: function () {
+        this.location();
+    },
 
-                $("#data").html("weather type: " + weatherType + ' ' + temp);
+    methods: {
+        location: function () {
+            $(document).ready(function () {
+                var lon;
+                var lat;
+                if (navigator.geolocation) {
+                    var gl = navigator.geolocation;
+                    gl.getCurrentPosition(function (position) {
+                        lat = position.coords.latitude;
+                        lon = position.coords.longitude;
+                        var key = '53f9d8e4213222cf517d86dc406d67fc';
+                        var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key + '&mode=json&units=metric';
+
+                        $.getJSON(api, function (data) {
+                            weatherType = data.weather[0].description;
+                            // weatherType.push(data.locationDetails[].weatherType);
+                            temp = data.main.temp;
+                            console.log(temp);
+                            console.log(weatherType);
+
+                            $("#weather").html("weather type: " + weatherType + ' ' + temp + ' ');
+                        });
+                    });
+                }
             });
-        });
+
+        }
     }
-});
+})
+
+// $(document).ready(function () {
+//     var lon;
+//     var lat;
+//     if (navigator.geolocation) {
+//         var gl = navigator.geolocation;
+//         gl.getCurrentPosition(function (position) {
+//             lat = position.coords.latitude;
+//             lon = position.coords.longitude;
+//             var key = '53f9d8e4213222cf517d86dc406d67fc';
+//             var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key + '&mode=json&units=metric';
+
+//             $.getJSON(api, function (data) {
+//                 var weatherType = data.weather[0].description;
+//                 var temp = data.main.temp;
+//                 console.log(temp);
+//                 console.log(weatherType);
+
+//                 $("#data").html("weather type: " + weatherType + ' ' + temp);
+//             });
+//         });
+//     }
+// });
 
