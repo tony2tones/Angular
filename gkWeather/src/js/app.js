1518e6f03660c018d9fa0eb2a32d3f505c64fcec
tony2tones <anthony.marque@gmail.com>
2017-12-07 18:21:04 +0200

    added Refresh weather post and added image switching with API icon codes.

--- gkWeather/src/js/app.js
+++ gkWeather/src/js/app.js
@@ -7,6 +7,7 @@ import Vue from 'vue';
             message: 'Hello guuys',
             errorMessage: false,
             loading: false,
+            imageLink: 'http://weclipart.com/gimg/CA61722230C10362/KcnEpgKcq.jpeg',
             apiKEY: '53f9d8e4213222cf517d86dc406d67fc',
             baseURL: 'http://api.openweathermap.org/data/2.5/weather',
             src: '',
@@ -27,7 +28,7 @@ import Vue from 'vue';
         },
 
         methods: {
-         
+
             location: function () {
                 // 1. get users location
                 // 2. and handle error
@@ -78,24 +79,21 @@ import Vue from 'vue';
             weatherIcon: function (iconName) {
                 // return iconName
                 this.src = iconName;
-                console.log(this.src);
-                if (iconName === '01d' || '01n') {
-                    $("#image").attr("src", "https://static.pexels.com/photos/557782/pexels-photo-557782.jpeg");
-                } else if (iconName === '02d' || '02n') {
-                    $("#image").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Cumulus_Clouds_Over_Jamaica.jpg/1280px-Cumulus_Clouds_Over_Jamaica.jpg");
-                }
-                else if (iconName === '03d' || '03n') {
-                    $("#image").attr("src", "https://pre00.deviantart.net/30aa/th/pre/f/2013/317/f/b/dark_rain_by_zyres-d6u4gcw.png");
-                } else if (iconName === '04d' || '04n') {
-                    //dark clouds
-                    $("#image").attr("src", "https://static.pexels.com/photos/557782/pexels-photo-557782.jpeg");
-                } else if (iconName === '09d' || '09n') {
-                    $("#image").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Cumulus_Clouds_Over_Jamaica.jpg/1280px-Cumulus_Clouds_Over_Jamaica.jpg");
-                } else if (iconName === '10d' || '10n') {
-                    $("#image").attr("src", "https://tctechcrunch2011.files.wordpress.com/2015/08/clouds.jpg");
-                } else if (iconName === '11d' || '11n') {
-                    //dark clouds
-                    $("#image").attr("src", "https://static.pexels.com/photos/557782/pexels-photo-557782.jpeg");
+                var self = this;
+                if (iconName === '01d' || iconName === '01n') {
+                    self.imageLink = "http://mikeiamele.com/wp-content/uploads/2014/05/Sunny-Day-800x450.jpg";
+                } else if (iconName === '02d' || iconName === '02n') {
+                    self.imageLink = "https://www.newvision.co.ug/w-images/6f544bf7-4a4a-46a0-be3d-f656bd88bd48/1/cloudy-703x422.jpg";
+                } else if (iconName === '03d' || iconName === '03n') {
+                    self.imageLink = "https://i.ytimg.com/vi/rRL_9WxBJBc/maxresdefault.jpg";
+                } else if (iconName === '04d' || iconName === '04n') {
+                    self.imageLink = "http://cdn.pcwallart.com/images/dark-clouds-wallpaper-4.jpg";
+                } else if (iconName === '09d' || iconName === '09n') {
+                    self.imageLink = "https://pbs.twimg.com/media/CJb-mE_VEAAu-WB.jpg";
+                } else if (iconName === '10d' || iconName === '10n') {
+                    self.imageLink = "https://tctechcrunch2011.files.wordpress.com/2015/08/clouds.jpg";
+                } else if (iconName === '11d' || iconName === '11n') {
+                    self.imageLink = "https://pbs.twimg.com/media/CfvNkjbWsAATGV8.jpg";
                 }
             },
             convertKelvinToCelcius: function (deg) {
